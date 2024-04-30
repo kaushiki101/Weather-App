7bd607f69b9b0524b460adb48b3bf556fcbf1698
kaushiki <kaushikisapce20>
2024-05-01 02:20:57 +0530

    first commit

--- my-app/src/component/WeatherApp/Tempapp.js
+++ my-app/src/component/WeatherApp/Tempapp.js
@@ -1,15 +1,13 @@
 import React, { useEffect, useState } from "react";
 import "./Tempapp.css";
-import sun from "../Assets/sun.png";
 import wind from "../Assets/wind.png";
-import snow from "../Assets/snow.png";
-// import clear from "../Assets/clear.png";
-import rainy from "../Assets/rainy.png";
-import cloud from "../Assets/cloud.png";
 import humidity from "../Assets/humidity.png";
+import Temperature from "../Assets/temperature.png";
+import sunset from "../Assets/sunset.png";
 
 const Tempapp = () => {
   const [city, setCity] = useState("");
+  const [cityName, setCityName] = useState("null");
   const [search, setSearch] = useState("Japan");
 
   useEffect(() => {
@@ -17,17 +15,38 @@ const Tempapp = () => {
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=cb2c4b1e584528fc98cf5a27a4539d2e`;
       const respnse = await fetch(url);
       const resjson = await respnse.json();
-      setCity(resjson.main);
+      setCity(resjson);
     };
     fetchapi();
-  }, [search]);
+  }, [cityName]);
 
   const handleLocationChange = (e) => {
     setSearch(e.target.value);
   };
 
+  const handleEnter = (e) => {
+    if (e.keyCode === 13) {
+      setCityName(search);
+    }
+  };
+
+  // if (city) {
+  //   console.log(city);
+  // }
+
   return (
     <div className="container">
+      <div className="sunset">
+        <img
+          src={sunset}
+          alt="sunset"
+          style={{
+            width: "675px",
+            height: "830px",
+            borderRadius: "10px",
+          }}
+        />
+      </div>
       <h1 className="Head">WEATHER APP</h1>
       <div className="top-bar">
         <div className="search-icon">
@@ -36,6 +55,8 @@ const Tempapp = () => {
             className="button"
             onChange={handleLocationChange}
             value={search}
+            onKeyDown={handleEnter}
+            placeholder="Search"
           />
         </div>
 
@@ -44,37 +65,49 @@ const Tempapp = () => {
         ) : (
           <div className="small-div">
             <div className="weather-image">
-              <img src={rainy} alt="rainy" />
+              <div className="element">
+                <h3 className="Main">
+                  <div>{city.weather[0].main}</div>
+                </h3>
+              </div>
+
+              <img
+                src={`http://openweathermap.org/img/w/${city.weather[0].icon}.png`}
+                alt={city.weather[0].description}
+              />
             </div>
-            <div className=""></div>
-            <h3 className="location">{search}</h3>
-            <h3 className="temp"> tempertaure {city.temp} </h3>
+            <div className="location">{search}</div>
+            <div className="element-1">
+              <h3 className="temp">Tempertaure:</h3>
+              <div> {city.main.temp}°F</div>
+              <img src={Temperature} alt=" Temperature" />
+            </div>
             <div className="data">
+              <button className="temper">
+                {" "}
+                <h1>{city.main.temp_min}</h1>
+                MIN TEMP{" "}
+              </button>
               <div className="element">
-                <div className="humdidty">
-                  <p>Humidity: {city.humidity}%</p>
-                  <img src={humidity} alt=" humidity" />
-                </div>
+                <h3 className="Main"> Humidity:</h3>
+                <div> {city.main.humidity}%</div>
+                <img src={humidity} alt=" humidity" />
+              </div>
+              <div className="element">
+                <h3 className="Main">Description:</h3>
+                <div>{city.weather[0].description}</div>
+              </div>
+              <div className="element">
+                <h3 className="Main">Wind:</h3>
+                <div>{city.wind.speed}km/h</div>
+                <img src={wind} alt="wind" />
               </div>
 
-              <div className="element">
-                <div className="wind">
-                  <p>Wind:{city.wind && console.log(city.wind)}</p>
-                  <img src={wind} alt="wind" />
-                </div>
-              </div>
+              <button className="temper">
+                <h1>{city.main.temp_max}</h1>
+                MAX TEMP{" "}
+              </button>
             </div>
-
-            <br></br>
-            <button className="temper">
-              {" "}
-              <h1>{city.temp_min}</h1>
-              MIN TEMP{" "}
-            </button>
-            <button className="temper">
-              <h1>{city.temp_max}</h1>
-              MAX TEMP{" "}
-            </button>
           </div>
         )}
       </div>
